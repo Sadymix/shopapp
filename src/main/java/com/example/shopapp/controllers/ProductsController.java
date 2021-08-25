687ceea9c0d4b56642c1a09b6cf466cdea7aa446
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-08-25 15:12:20 +0200

    added product controller

---
+++ src/main/java/com/example/shopapp/controllers/ProductsController.java
@@ -0,0 +1,29 @@
+package com.example.shopapp.controllers;
+
+import com.example.shopapp.models.Product;
+import com.example.shopapp.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/shop")
+public class ProductsController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public String listProducts(Model model) {
+
+        List<Product> theProducts = productService.getProducts();
+
+        model.addAttribute("products", theProducts);
+
+        return "product-list";
+    }
+}
