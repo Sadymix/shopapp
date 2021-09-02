dbb8be32c49a0e9fbe312356d8072fafc0d4710f
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-02 15:07:39 +0200

    removed service interfaces, and changed the nomenclature of the ServiceImpl to Service

--- src/main/java/com/example/shopapp/services/CartService.java
+++ src/main/java/com/example/shopapp/services/CartService.java
@@ -2,10 +2,48 @@ package com.example.shopapp.services;
 
 import com.example.shopapp.dto.CartDTO;
 import com.example.shopapp.dto.ProductDTO;
+import com.example.shopapp.mappers.ProductMapper;
+import com.example.shopapp.models.Product;
+import com.example.shopapp.repositories.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface CartService {
-    public List<ProductDTO> addProductsToBasket(CartDTO theProductIds);
+@Service
+@RequiredArgsConstructor
+public class CartService {
 
+    private final ProductRepo productRepo;
+    private final ProductMapper productMapper;
+
+    public List<ProductDTO> getProductsByIds(CartDTO theProductIds) {
+        String s = theProductIds.getProductIds();
+
+        String[] productIdsArr = s.split(",");
+        var productIdsList = Arrays.stream(productIdsArr)
+                .map(Long::parseLong)
+                .distinct()
+                .collect(Collectors.toList());
+        var products = productRepo.findAllById(productIdsList);
+
+        return Arrays.stream(productIdsArr)
+                .map(Long::parseLong)
+                .map(id -> getProductWithId(id, products))
+                .map(productMapper::toDto)
+                .toList();
+    }
+
+    private Product getProductWithId(Long id, Iterable<Product> iterableProduct) {
+
+        Product theProduct = new Product();
+        for(Product product: iterableProduct) {
+            if(product.getProductId() == id) {
+                theProduct= product;
+            }
+        }
+        return theProduct;
+    }
 }
--- src/main/java/com/example/shopapp/services/ProductService.java
+++ src/main/java/com/example/shopapp/services/ProductService.java
@@ -1,15 +1,30 @@
 package com.example.shopapp.services;
 
 import com.example.shopapp.dto.ProductDTO;
+import com.example.shopapp.mappers.ProductMapper;
+import com.example.shopapp.models.Product;
+import com.example.shopapp.repositories.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
+public class ProductService {
 
-public interface ProductService {
+    private final ProductRepo productRepo;
 
+    private final ProductMapper productMapper;
 
-    public List<ProductDTO> getAllProducts();
+    public List<ProductDTO> getAllProducts() {
 
+        List<Product> products = (List<Product>) productRepo.findAll();
+        return products.stream()
+                .map(productMapper::toDto)
+                .collect(Collectors.toList());
+    }
 
 
 }
