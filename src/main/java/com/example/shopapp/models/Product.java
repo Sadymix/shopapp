8bacc751830d3ed7ee783997985c9fe20c821502
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-07 16:40:22 +0200

    added lombok @Builder

--- src/main/java/com/example/shopapp/models/Product.java
+++ src/main/java/com/example/shopapp/models/Product.java
@@ -2,6 +2,7 @@ package com.example.shopapp.models;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "products")
+@Builder
 public class Product {
 
     @Id
