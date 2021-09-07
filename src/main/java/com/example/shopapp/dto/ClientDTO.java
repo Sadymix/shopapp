5a1bf66d88ec8a0133536b4d84ef9c0f7509cafc
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-07 14:43:05 +0200

    added ClientDTO

---
+++ src/main/java/com/example/shopapp/dto/ClientDTO.java
@@ -0,0 +1,36 @@
+package com.example.shopapp.dto;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO{
+
+    private Long clientId;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String postalCode;
+
+    @NotNull
+    private String country;
+
+}
