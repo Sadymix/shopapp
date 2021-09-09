c96689d04fce6a2bb7b23e26df75f2730df535ca
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-09 15:07:52 +0200

    added NoAgsConstructor for the class

--- src/main/java/com/example/shopapp/dto/ProductDTO.java
+++ src/main/java/com/example/shopapp/dto/ProductDTO.java
@@ -4,10 +4,12 @@ import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
 
