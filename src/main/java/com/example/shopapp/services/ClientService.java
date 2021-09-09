24f45845d50ffc38db188d70757c4acae435a355
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-09 14:47:45 +0200

    removed not used method getOrderProductsByIds, changed getOrderTotalPrice (now its using stream), injected CartService and used method getProductsByIds

--- src/main/java/com/example/shopapp/services/ClientService.java
+++ src/main/java/com/example/shopapp/services/ClientService.java
@@ -1,18 +1,15 @@
 package com.example.shopapp.services;
 
-import com.example.shopapp.dto.ProductDTO;
 import com.example.shopapp.mappers.ClientMapper;
 import com.example.shopapp.mappers.ProductMapper;
 import com.example.shopapp.models.Order;
 import com.example.shopapp.models.Product;
 import com.example.shopapp.repositories.ClientRepo;
 import com.example.shopapp.repositories.OrderRepo;
-import com.example.shopapp.repositories.ProductRepo;
 import com.example.shopapp.wrappers.ClientWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -24,17 +21,16 @@ public class ClientService {
 
     private final ClientRepo clientRepo;
 
-    private final ProductRepo productRepo;
-
     private final ProductMapper productMapper;
 
     private final OrderRepo orderRepo;
 
-    public void addClientAndOrder(ClientWrapper clientWrapper) {
+    private final CartService cartService;
 
+    public void addClientAndOrder(ClientWrapper clientWrapper) {
         var client = clientMapper.toEntity(clientWrapper.getClient());
         var orderClient = clientRepo.save(client);
-        var orderProductsDTOList = getOrderProductsByIds(clientWrapper);
+        var orderProductsDTOList = cartService.getProductsByIds(clientWrapper.getOrderProductIds());
         var orderProductsList = orderProductsDTOList.stream()
                 .map(productMapper::toEntity)
                 .toList();
@@ -47,39 +43,11 @@ public class ClientService {
         orderRepo.save(order);
     }
 
-    public List<ProductDTO> getOrderProductsByIds(ClientWrapper clientWrapper) {
-
-        var theProductIdsString = clientWrapper.getOrderProductIds();
-        var arr = theProductIdsString.split(",");
-        var productIdsList = Arrays.stream(arr)
-                .map(Long::parseLong)
-                .distinct()
-                .toList();
-        var products = productRepo.findAllById(productIdsList);
-
-        return Arrays.stream(arr)
-                .map(Long::parseLong)
-                .map(id -> getProductWithIds(id, products))
-                .map(productMapper::toDto)
-                .toList();
-    }
-
-    private Product getProductWithIds(Long id, Iterable<Product> iterableProduct) {
-
-        for (Product product : iterableProduct) {
-            if (product.getProductId().equals(id)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
     private Double getOrderTotalPrice(List<Product> orderProductsList) {
-        double totalPrice = 0.0;
-        for (Product product : orderProductsList) {
-            totalPrice += product.getPrice();
-        }
-        return totalPrice;
+        return orderProductsList.stream()
+                .map(Product::getPrice)
+                .mapToDouble(Double::doubleValue)
+                .sum();
     }
 
 }
