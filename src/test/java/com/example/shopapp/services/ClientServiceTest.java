7dcf5453d872f95b84b9e82c20cc5375e06889dd
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-09 14:59:52 +0200

    added static final fields for Client, and for 3 Products, removed productRepo mock, mocked cartService, removed unused data from setUp, change mocks for ClientMapper, ClientRepo, productMapper, removed unused data in testAddClientAndOrder(), added addClientAndOrder method in testAddClientOrder, changed verify method and removed asserts, removed mockProduct, stubbingSetUp, orderSetUp, assertEqualsIds, assertEqualsIds, assertEqualsPrices, orderProductsIdListStUp, getTotalPrice, toEntity

--- src/test/java/com/example/shopapp/services/ClientServiceTest.java
+++ src/test/java/com/example/shopapp/services/ClientServiceTest.java
@@ -9,7 +9,6 @@ import com.example.shopapp.models.Order;
 import com.example.shopapp.models.Product;
 import com.example.shopapp.repositories.ClientRepo;
 import com.example.shopapp.repositories.OrderRepo;
-import com.example.shopapp.repositories.ProductRepo;
 import com.example.shopapp.wrappers.ClientWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,26 +17,40 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ClientServiceTest {
 
-    @Mock
-    private ClientRepo clientRepo;
+    private static final Client CLIENT = new Client(null, "Michael", "Jordan", "Warsaw",
+            "Prosta", "50-500", "Poland", null);
+
+    private static final Product PRODUCT1 = Product.builder()
+            .productId(1L)
+            .price(1.5)
+            .build();
+    private static final Product PRODUCT2 = Product.builder()
+            .productId(2L)
+            .price(3.0)
+            .build();
+    private static final Product PRODUCT3 = Product.builder()
+            .productId(3L)
+            .price(5.5)
+            .build();
+
     @Mock
     private ClientMapper clientMapper;
     @Mock
-    private ProductRepo productRepo;
+    private ClientRepo clientRepo;
     @Mock
     private ProductMapper productMapper;
     @Mock
     private OrderRepo orderRepo;
+    @Mock
+    private CartService cartService;
 
     @InjectMocks
     private ClientService tested;
@@ -45,57 +58,27 @@ class ClientServiceTest {
     @BeforeEach
     void setUp() {
         var clientWrapper = clientWrapperSetUp();
-        var theArrayOfOrderProductIds = clientWrapper.getOrderProductIds().split(",");
-        var productIdsList = Arrays.stream(theArrayOfOrderProductIds)
-                .map(Long::parseLong)
-                .distinct()
-                .toList();
-        when(productMapper.toDto(any(Product.class))).thenAnswer(invocationOnMock -> {
-            Product product = invocationOnMock.getArgument(0);
-            return ProductDTO.builder().productId(product.getProductId()).price(product.getPrice()).build();
+        when(clientMapper.toEntity(any(ClientDTO.class))).thenReturn(CLIENT);
+        when(clientRepo.save(CLIENT)).thenAnswer(invocationOnMock -> {
+            Client c = invocationOnMock.getArgument(0);
+            c.setClientId(1L);
+            return c;
         });
-        doReturn(List.of(mockProduct(1L, 1.5),
-                mockProduct(2L, 3.0),
-                mockProduct(3L, 5.5),
-                mockProduct(4L, 4.5),
-                mockProduct(5L, 5.0)))
-                .when(productRepo).findAllById(productIdsList);
-    }
-
-    @Test
-    void testGetOrderProductsByIds() {
-        var clientWrapper = clientWrapperSetUp();
-        var orderProductIdsList = orderProductsIdListSetUp(clientWrapper);
-
-        var orderProductsDTO = tested.getOrderProductsByIds(clientWrapper);
-
-        assertEquals(6, orderProductsDTO.size());
-        verify(productRepo).findAllById(orderProductIdsList);
-        verify(productMapper, times(6)).toDto(any(Product.class));
-        assertEqualsIds(orderProductsDTO);
-
+        when(cartService.getProductsByIds(clientWrapper.getOrderProductIds()))
+                .thenReturn(List.of(new ProductDTO(), new ProductDTO(), new ProductDTO()));
+        when(productMapper.toEntity(any(ProductDTO.class)))
+                .thenReturn(PRODUCT1, PRODUCT2, PRODUCT3);
     }
 
     @Test
     void testAddClientAndOrder() {
         var clientWrapper = clientWrapperSetUp();
-        var client = toEntity(clientWrapper.getClient());
-        stubbingSetUp(client);
-        Order order = orderSetUp(clientWrapper, client);
 
-        verify(productMapper, times(12)).toEntity(any(ProductDTO.class));
-        verify(clientRepo, atLeast(0)).save(client);
-        verify(orderRepo, atLeast(0)).save(order);
-        assertEqualsIds(order);
-        assertEqualsPrices(order);
-    }
-
-    private Product mockProduct(Long id, Double price) {
-
-        var product = mock(Product.class);
-        doReturn(id).when(product).getProductId();
-        doReturn(price).when(product).getPrice();
-        return product;
+        tested.addClientAndOrder(clientWrapper);
+        verify(clientMapper).toEntity(any(ClientDTO.class));
+        verify(clientRepo).save(CLIENT);
+        verify(productMapper, times(3)).toEntity(any(ProductDTO.class));
+        verify(orderRepo).save(any(Order.class));
     }
 
     private ClientWrapper clientWrapperSetUp() {
@@ -107,84 +90,5 @@ class ClientServiceTest {
         clientWrapper.setClient(clientDTO);
 
         return clientWrapper;
-    }
-
-    private void stubbingSetUp(Client client) {
-        when(clientMapper.toEntity(any(ClientDTO.class)))
-                .thenReturn(Client.builder().build());
-        when(clientRepo.save(any(Client.class))).thenReturn(client);
-        when(productMapper.toEntity(any(ProductDTO.class))).thenAnswer(invocationOnMock -> {
-            ProductDTO productDTO = invocationOnMock.getArgument(0);
-            return Product.builder().productId(productDTO.getProductId()).price(productDTO.getPrice()).build();
-        });
-    }
-
-    private Order orderSetUp(ClientWrapper clientWrapper, Client client) {
-        var productsDTOList = tested.getOrderProductsByIds(clientWrapper);
-        var productsList = productsDTOList.stream()
-                .map(productMapper::toEntity)
-                .toList();
-        var totalPrice = getTotalPrice(productsList);
-        var order = new Order(1L, totalPrice, productsList, client);
-        when((orderRepo).save(any(Order.class))).thenReturn(order);
-        tested.addClientAndOrder(clientWrapper);
-
-        return order;
-    }
-
-    private void assertEqualsIds(Order order) {
-        assertEquals(1L, order.getProductList().get(0).getProductId());
-        assertEquals(4L, order.getProductList().get(1).getProductId());
-        assertEquals(3L, order.getProductList().get(2).getProductId());
-        assertEquals(2L, order.getProductList().get(3).getProductId());
-        assertEquals(5L, order.getProductList().get(4).getProductId());
-        assertEquals(5L, order.getProductList().get(5).getProductId());
-    }
-
-    private void assertEqualsIds(List<ProductDTO> orderProductsDTOList) {
-        assertEquals(1L, orderProductsDTOList.get(0).getProductId());
-        assertEquals(4L, orderProductsDTOList.get(1).getProductId());
-        assertEquals(3L, orderProductsDTOList.get(2).getProductId());
-        assertEquals(2L, orderProductsDTOList.get(3).getProductId());
-        assertEquals(5L, orderProductsDTOList.get(4).getProductId());
-        assertEquals(5L, orderProductsDTOList.get(5).getProductId());
-    }
-
-    private void assertEqualsPrices(Order order) {
-        assertEquals(1.5, order.getProductList().get(0).getPrice());
-        assertEquals(4.5, order.getProductList().get(1).getPrice());
-        assertEquals(5.5, order.getProductList().get(2).getPrice());
-        assertEquals(3.0, order.getProductList().get(3).getPrice());
-        assertEquals(5.0, order.getProductList().get(4).getPrice());
-        assertEquals(5.0, order.getProductList().get(5).getPrice());
-    }
-
-    private List<Long> orderProductsIdListSetUp(ClientWrapper clientWrapper) {
-        String[] orderProductIdsArr = clientWrapper.getOrderProductIds().split(",");
-        var orderProductIdsList = Arrays.stream(orderProductIdsArr)
-                .map(Long::parseLong)
-                .distinct()
-                .toList();
-        return orderProductIdsList;
-    }
-
-    private Double getTotalPrice(List<Product> productList) {
-        var totalPrice = 0.0;
-        for (Product product : productList) {
-            totalPrice += product.getPrice();
-        }
-        return totalPrice;
-    }
-
-    private Client toEntity(ClientDTO clientDTO) {
-        return Client.builder()
-                .clientId(clientDTO.getClientId())
-                .firstName(clientDTO.getFirstName())
-                .lastName(clientDTO.getLastName())
-                .city(clientDTO.getCity())
-                .street(clientDTO.getStreet())
-                .postalCode(clientDTO.getPostalCode())
-                .country(clientDTO.getCountry())
-                .build();
     }
 }
