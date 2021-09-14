bbdad82398a458d2ff31b11e7567ae764ef9d235
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-14 16:25:23 +0200

    added method toEntity that builds Order from OrderDTO

--- src/main/java/com/example/shopapp/mappers/OrderMapper.java
+++ src/main/java/com/example/shopapp/mappers/OrderMapper.java
@@ -2,14 +2,26 @@ package com.example.shopapp.mappers;
 
 import com.example.shopapp.dto.OrderDTO;
 import com.example.shopapp.models.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderMapper {
 
-    public OrderDTO toDTO(Order orderEntity) {
-        return OrderDTO.builder()
-                .orderId(orderEntity.getOrderId())
+    private final ProductMapper productMapper;
+    private final ClientMapper clientMapper;
+
+    public Order toEntity(OrderDTO orderDTO) {
+        var productsDTOList = orderDTO.getProducts();
+        var productsList = productsDTOList.stream()
+                .map(productMapper::toEntity)
+                .toList();
+        return Order.builder()
+                .orderId(orderDTO.getOrderId())
+                .productList(productsList)
+                .totalPrice(orderDTO.getTotalPrice())
+                .client(clientMapper.toEntity(orderDTO.getClientDTO()))
                 .build();
     }
 }
