7207a355a67e5fc86a8a8f7866da61dfa1f03805
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-16 12:04:51 +0200

    added mocks for productMapper and clientMapper inject mocks into OrderMapper added static verify method for testing productMapper and clientMapper

--- src/test/java/com/example/shopapp/mappers/OrderMapperTest.java
+++ src/test/java/com/example/shopapp/mappers/OrderMapperTest.java
@@ -1,17 +1,30 @@
 package com.example.shopapp.mappers;
+
 import com.example.shopapp.models.Client;
 import com.example.shopapp.models.Order;
 import com.example.shopapp.models.Product;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class OrderMapperTest {
-    private final ProductMapper productMapper = new ProductMapper();
-    private final ClientMapper clientMapper = new ClientMapper();
-    private final OrderMapper tested = new OrderMapper(productMapper, clientMapper);
+    @Mock
+    private ProductMapper productMapper;
+    @Mock
+    private ClientMapper clientMapper;
+    @InjectMocks
+    private OrderMapper tested;
 
     private static final Product PRODUCT1 = Product.builder()
             .productId(1L)
@@ -28,17 +41,17 @@ class OrderMapperTest {
     private static final Client CLIENT = new Client(5L, "Michael", "Jordan", "Warsaw",
             "Prosta", "50-500", "Poland", null);
 
-
     @Test
     void testToDTO() {
         var productList = List.of(PRODUCT1, PRODUCT2, PRODUCT3);
         var order = new Order(12L, 32.5, productList, CLIENT);
 
         var orderDTO = tested.toDTO(order);
-
         assertNotNull(orderDTO);
         assertEquals(order.getOrderId(), orderDTO.getOrderId());
         assertEquals(order.getTotalPrice(), orderDTO.getTotalPrice());
+        verify(productMapper, times(3)).toDto(any(Product.class));
+        verify(clientMapper).toDTO(any(Client.class));
     }
 
 }
