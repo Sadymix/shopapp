33e1f03d93e96db98fc1f43817ca5840298dc768
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-09-16 11:13:21 +0200

    added addResourceHandlers method to find the webapp files outside of src dir

--- src/main/java/com/example/shopapp/config/WebConfig.java
+++ src/main/java/com/example/shopapp/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.example.shopapp.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/")
+                .setCachePeriod(0);
     }
 }
