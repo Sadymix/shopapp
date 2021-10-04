f4bd036ecaec1c72b893a78a4ff41f035961bd31
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-04 14:21:00 +0200

    added UserDao field added constructor added loadUserByUserName, which returns the user or throw an exception

---
+++ src/main/java/com/example/shopapp/auth/UserService.java
@@ -0,0 +1,25 @@
+package com.example.shopapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userDao.selectUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
+    }
+}
