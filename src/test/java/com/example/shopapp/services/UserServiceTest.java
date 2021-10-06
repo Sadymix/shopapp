8b8efaec181160c4342cdad1acc42284ae747a3b
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-06 14:18:54 +0200

    added UserServiceTest unit test added static final User field (builder) mocked user added setup method - stubbed userRepo added testLoadUserByUsername method, which verifies number of use findByUsername method and checks that username is the same after loadUserByUsername method

---
+++ src/test/java/com/example/shopapp/services/UserServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.shopapp.services;
+
+import com.example.shopapp.models.User;
+import com.example.shopapp.repositories.UserRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    private static final User USER = User.builder()
+            .username("user")
+            .build();
+    @Mock
+    private UserRepo userRepo;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        doReturn(USER)
+                .when(userRepo).findByUsername("user");
+    }
+
+    @Test
+    void testLoadUserByUsername() {
+        var user = userService.loadUserByUsername("user");
+        verify(userRepo).findByUsername("user");
+        assertEquals("user", user.getUsername());
+    }
+}
