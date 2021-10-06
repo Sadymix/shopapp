ea48c40748d2a00162eed17b3ed046495b513d21
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-06 16:03:16 +0200

    added setUp method for MockMvc modified testGetProductList method (basic auth), added @WithMockUser annotation added @WithMockUser to testGetCartProductList

--- src/test/java/com/example/shopapp/controllers/WebMvcProductControllerTest.java
+++ src/test/java/com/example/shopapp/controllers/WebMvcProductControllerTest.java
@@ -1,15 +1,22 @@
 package com.example.shopapp.controllers;
 
 import com.example.shopapp.dto.ProductDTO;
-import com.example.shopapp.repositories.ProductRepo;
+import com.example.shopapp.models.User;
+import com.example.shopapp.repositories.UserRepo;
 import com.example.shopapp.services.CartService;
 import com.example.shopapp.services.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
@@ -18,22 +25,13 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
 class WebMvcProductControllerTest {
-
-    @Autowired
-    private MockMvc mvc;
-
-    @MockBean
-    private ProductService productService;
-    @MockBean
-    private ProductRepo productRepo;
-    @MockBean
-    private CartService cartService;
 
     private static final ProductDTO PRODUCT_DTO1 = ProductDTO.builder()
             .productId(1L)
@@ -48,14 +46,43 @@ class WebMvcProductControllerTest {
             .price(5.5)
             .build();
 
+    private static final User USER = User.builder()
+            .username("user")
+            .password("password")
+            .build();
+
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private ProductService productService;
+    @MockBean
+    private CartService cartService;
+    @MockBean
+    private UserRepo userRepo;
+
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser(value = "test", password = "password")
     @Test
     public void testGetProductsList() throws Exception{
+
         ProductDTO productDTO = new ProductDTO(2L, "milk", 5.0);
         var productDTOList = List.of(productDTO);
 
         given(productService.getAllProducts()).willReturn(productDTOList);
 
-        mvc.perform(get("/shop/products"))
+        mvc.perform(get("/shop/products").header(HttpHeaders.AUTHORIZATION,
+                        "Basic" + Base64Utils.encodeToString(("user:secret".getBytes()))))
                 .andExpect(content().contentType(APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
@@ -65,6 +92,7 @@ class WebMvcProductControllerTest {
         verify(productService).getAllProducts();
     }
 
+    @WithMockUser(username = "username", password = "password", roles = "USER")
     @Test
     public void testGetCartProductList() throws Exception{
         var productIdsString = "2,1,3,2";
