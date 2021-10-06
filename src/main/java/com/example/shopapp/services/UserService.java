a7734ecb4893a4d5797d10cc70fbf038c8d8750b
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-10-06 13:41:45 +0200

    moved UserService.java to services package removed @SupressWarnings changed UserDao to UserRepo modified loadUserByUserName method

--- src/main/java/com/example/shopapp/services/UserService.java
+++ src/main/java/com/example/shopapp/services/UserService.java
@@ -1,25 +1,24 @@
-package com.example.shopapp.auth;
+package com.example.shopapp.services;
 
+import com.example.shopapp.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Service
 public class UserService implements UserDetailsService {
 
-    private final UserDao userDao;
+    private UserRepo userRepo;
 
     @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userDao.selectUserByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
+        return userRepo.findByUsername(username);
     }
 }
